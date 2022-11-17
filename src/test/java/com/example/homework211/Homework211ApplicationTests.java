7dcc4a6e33d1e6ce19fe5f405aaa4b6793bf7ce8
Pryanikspb <konstantinprivator@gmail.com>
2022-11-17 12:46:22 +0300

    Initial commit

---
+++ src/test/java/com/example/homework211/Homework211ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework211;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
